2b6b73fa5010eca00574275adafec86e82c88bbf
sil-28 <silvbg028@gmail.com>
2026-09-25 19:15:14 +0200

    main loop

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-
-
+use std::io;
 struct Task {
     description: String,
     done: bool
@@ -22,6 +21,7 @@ fn remove_task(list: &mut Vec<Task>, index: usize){
 }
 
 fn print_list(list: &Vec<Task>){
+    println!("");
     for (i,task) in list.iter().enumerate(){
         let status = if task.done {"x"} else {""};
         println!("{}. [{}] {}", i, status, task.description);
@@ -32,7 +32,58 @@ fn print_list(list: &Vec<Task>){
 fn main() {
     let mut list: Vec<Task> = Vec::new();
 
+    loop{
+        println!("\n --- TO DO LIST ---");
+        println!("1. Add task");
+        println!("2. Complete task");
+        println!("3. Remove task");
+        println!("4. Show list");
+        println!("5. Exit");
+        println!("Choose an option:");
 
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read line");
+        
+        let input = input.trim();
+
+        match input {
+            "1" => {
+                println!("Enter description of new task");
+                let mut description = String::new();
+                io::stdin().read_line(&mut description).expect("Failed to read line");
+                add_task(&mut list, description.trim().to_string());
+                },
+            "2" => {
+                println!("Enter task index to complete");
+                let mut index = String::new();
+                io::stdin().read_line(&mut index).expect("Failed to read line");
+                let index: usize = index.trim().parse().unwrap();
+                complete_task(&mut list, index);
+                },
+            "3" => {
+                println!("Enter task index to remove");
+                let mut index = String::new();
+                io::stdin().read_line(&mut index).expect("Failed to read line");
+                let index: usize = index.trim().parse().unwrap();
+                remove_task(&mut list, index);
+                },
+            "4" => {
+                print_list(&list);
+                },
+            "5" => {
+                println!("Goodbye!");
+                break;
+                },
+            _ => {
+                println!("Invalid option. Choose again!");
+            }
+        };
+
+
+
+
+
+    }
 
 
 }
